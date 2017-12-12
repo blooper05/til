d82b9e1ff92bc82338f380f46f156073f4ab8c48
blooper05 <legend.of.blooper@gmail.com>
2017-12-12 15:01:24 +0900

    :pencil2: Basic of String

--- golang/An Introduction to Programming in Go/chapter3/main.go
+++ golang/An Introduction to Programming in Go/chapter3/main.go
@@ -6,4 +6,9 @@ func main() {
 	// 3.1 Numbers
 	fmt.Println("1 + 1 =", 1+1)
 	fmt.Println("1.0 + 1.0 =", 1.0+1.0)
+
+	// 3.2 Strings
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
 }
