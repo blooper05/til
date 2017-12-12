8b44e7f5d8fb6bdd200c0aa3ab5f8f38ef1777f9
blooper05 <legend.of.blooper@gmail.com>
2017-12-12 15:08:35 +0900

    :ok_man: Basic of Boolean

--- golang/An Introduction to Programming in Go/chapter3/main.go
+++ golang/An Introduction to Programming in Go/chapter3/main.go
@@ -11,4 +11,11 @@ func main() {
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[1])
 	fmt.Println("Hello " + "World")
+
+	// 3.3 Booleans
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
