39e4eff2801d91bbec262fbc227491914f067877
blooper05 <legend.of.blooper@gmail.com>
2020-04-08 20:02:59 +0900

    :shell: yarn run nest generate service cats

--- Node/Documentation | NestJS/src/app.module.ts
+++ Node/Documentation | NestJS/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
 import { AccountController } from './account/account.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController, AdminController, AccountController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
