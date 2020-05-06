a24a08b46fc30a251b3124bcfe7a940982bd26f8
blooper05 <legend.of.blooper@gmail.com>
2020-05-06 18:14:36 +0900

    :recycle: Move CatsController and CatsService to CatsModule

--- Node/Documentation | NestJS/src/app.module.ts
+++ Node/Documentation | NestJS/src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
 import { AccountController } from './account/account.controller';
-import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [CatsModule],
-  controllers: [AppController, CatsController, AdminController, AccountController],
-  providers: [AppService, CatsService],
+  controllers: [AppController, AdminController, AccountController],
+  providers: [AppService],
 })
 export class AppModule {}
