0ea5c48baae029a4c9a1abdfac9605e97f1b6b28
blooper05 <legend.of.blooper@gmail.com>
2020-05-06 18:39:56 +0900

    :+1: Restrict LoggerMiddleware only for GET method

--- Node/Documentation | NestJS/src/app.module.ts
+++ Node/Documentation | NestJS/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +13,6 @@ import { CatsModule } from './cats/cats.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes({ path: 'cats', method: RequestMethod.GET });
   }
 }
