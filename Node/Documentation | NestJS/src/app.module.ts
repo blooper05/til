66e564bca461f893c81c1914453264c1d2a988ac
blooper05 <legend.of.blooper@gmail.com>
2020-05-07 13:09:48 +0900

    :+1: Move global-scoped HttpExceptionFilter to AppModule

--- Node/Documentation | NestJS/src/app.module.ts
+++ Node/Documentation | NestJS/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { logger } from './common/middleware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,7 +11,13 @@ import { CatsModule } from './cats/cats.module';
 @Module({
   imports: [CatsModule],
   controllers: [AppController, AdminController, AccountController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
