076b71a5bdfaa1033db8f432535f6979f0d6a091
blooper05 <legend.of.blooper@gmail.com>
2020-05-27 17:35:55 +0900

    :+1: Add button action

--- Chatbot/Getting started with Bolt for JavaScript/src/index.ts
+++ Chatbot/Getting started with Bolt for JavaScript/src/index.ts
@@ -9,7 +9,26 @@ const app = new App({
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+  await say({
+    text: '',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me',
+          },
+          action_id: 'button_click',
+        },
+      },
+    ],
+  });
 });
 
 (async () => {
