f07f659f53826ad5edd158d4847b56a22e97c303
blooper05 <legend.of.blooper@gmail.com>
2020-05-27 16:38:06 +0900

    :+1: Fix errors of @typescript-eslint

--- Chatbot/Getting started with Bolt for JavaScript/src/index.ts
+++ Chatbot/Getting started with Bolt for JavaScript/src/index.ts
@@ -1,9 +1,9 @@
-const { App } = require('@slack/bolt');
+import { App } from '@slack/bolt';
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 (async () => {
